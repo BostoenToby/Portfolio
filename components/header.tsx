b44423b45e7a099f304b79eae964257a8d7b367f
Toby35 <toby.bostoen123@gmail.com>
2022-09-06 12:32:37 +0200

    sidebar + index responsive

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,6 @@ import Link from "../node_modules/next/link"
 import { getStorage, setStorage } from "./localstorage"
 
 export default (params) => {
-    // const [dark, setDark] = useState<boolean>()
     const [fullSize, setFullSize] = useState<boolean>(true)
     const [showSideNav, setShowSideNav] = useState<boolean>(false)
 
@@ -41,7 +40,7 @@ export default (params) => {
         setShowSideNav(!showSideNav)
     }
     return(
-        <div>
+        <div className="z-10">
             {fullSize? (
                 <header className="flex justify-between items-center mx-8 pt-6 font-montserrat">
                     <h1 className={`${params.dark? 'text-white border-white' : 'text-black border-black'} border-b-2 font-montserrat h-7 sm:text-sm`}>Bostoen Toby</h1>
@@ -59,20 +58,22 @@ export default (params) => {
                 <div>
                     <header className={`flex items-center mx-8 pt-6 font-montserrat ${showSideNav? 'justify-end' : 'justify-between'}`}>
                         <Menu className={`${params.dark? 'text-white':'text-black'} ${showSideNav? 'hidden' : ''}`} onClick={() => handleSideBar()}/>
-                        <div className={`fixed top-0 left-0 ${showSideNav? '' : 'hidden'} ${params.dark? 'bg-darkgray' : 'bg-lightgray'} h-screen w-1/3`}>
+                        <div className={`fixed top-0 left-0 ${showSideNav? '' : 'hidden'} ${params.dark? 'bg-darkgray' : 'bg-lightgray'} h-screen w-2/5`}>
                             <div className="h-screen flex flex-col justify-between py-6">
                                 <div>
-                                    <div className="flex justify-between mx-8 mt-6 items-center">
-                                        <h3 className={`${params.dark? 'text-lightblue' : 'text-green'} text-xl`}>Dashboard</h3>
-                                        <X className={`${params.dark? 'text-white' : 'text-black'} h-4 w-4`} onClick={() => handleSideBar()}/>
+                                    <div className="flex justify-between items-center mx-4 md:mx-8">
+                                        <X className={`${params.dark? 'text-white' : 'text-black'} h-6 w-6`} onClick={() => handleSideBar()}/>
+                                        <Sun className={`cursor-pointer ${params.dark? 'text-lightgray' : 'text-darkgray'} h-6 w-6`} onClick={async() => {changeTheme()}}/>
                                     </div>
-                                    <section className="mt-8 mx-8 space-y-4">
-                                        <Link href="/"><p className={`text-base ${params.dark? 'text-white' : 'text-black'} cursor-pointer`}>Home</p></Link>
-                                        <Link href="/projects"><p className={`text-base ${params.dark? 'text-white' : 'text-black'} cursor-pointer`}>Projects</p></Link>
-                                        <Link href="/about"><p className={`text-base ${params.dark? 'text-white' : 'text-black'} cursor-pointer`}>About</p></Link>
+                                    <div className="flex justify-between mx-4 mt-3 md:mx-8 md:mt-6 items-center">
+                                        <h3 className={`${params.dark? 'text-lightblue' : 'text-green'} text-lg md:text-xl`}>Dashboard</h3>
+                                    </div>
+                                    <section className="mt-4 mx-4 md:mt-8 md:mx-8 space-y-4">
+                                        <Link href="/"><p className={`text-base ${params.dark? 'text-white' : 'text-black'} text-base cursor-pointer`}>Home</p></Link>
+                                        <Link href="/projects"><p className={`text-base ${params.dark? 'text-white' : 'text-black'} text-base cursor-pointer`}>Projects</p></Link>
+                                        <Link href="/about"><p className={`text-base ${params.dark? 'text-white' : 'text-black'} text-base cursor-pointer`}>About</p></Link>
                                     </section>
                                 </div>
-                                <div className="flex justify-end"><Sun className={`cursor-pointer ${params.dark? 'text-lightgray' : 'text-darkgray'} mx-8 mb-16`} onClick={async() => {changeTheme()}}/></div>
                             </div>
                         </div>
                         <h1 className={`${params.dark? 'text-white border-white' : 'text-black border-black'} border-b-2 font-montserrat h-7 sm:text-sm`}>Bostoen Toby</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,35 @@ export default function Home() {
   const [dark, setDark] = useState<boolean>()
 
   const languages = [
-    "javascript",
-    "python",
-    "C#",
+    "Javascript",
+    "Python",
+    "NextJS",
     "React",
     "Gatsby",
     "TailwindCSS",
-    "NextJS"
+    "C#"
   ]
 
   return (
-    <div className={`h-screen ${dark? 'bg-black': 'bg-white'}`}>
+    <div className={`h-screen overflow-x-hidden overflow-y-hidden ${dark? 'bg-black': 'bg-white'} z-0`}>
       <Header setDark={setDark} dark={dark}/>
-      <main className="font-montserrat my-16 mx-8 space-y-4²">
-        <section className="grid grid-cols-2">
-          <div className="px-4 text-center py-40 flex flex-col">
-            <div className="items-start mb-16">
-              <h1 className={`${dark? 'text-white' : 'text-black'} text-5xl pb-4`}>Toby Bostoen</h1>
-              <h3 className={`${dark? 'text-white' : 'text-black'} text-3xl leading-normal`}>Frontend Developer<br />Based in Belgium</h3>
+      <main className="font-montserrat my-auto pt-4 mx-8 space-y-4">
+        <section className="sm:grid sm:grid-rows-2 md:grid-cols-2 md:grid-rows-none">
+          <div className="md:col-start-2 md:px-4 text-center py-40 flex flex-col my-auto">
+            <div className="mb-16">
+              <h1 className={`${dark? 'text-white' : 'text-black'} text-3xl md:text-5xl pb-4`}>Toby Bostoen</h1>
+              <h3 className={`${dark? 'text-white' : 'text-black'} text-xl md:text-3xl leading-normal`}>Frontend Developer<br />Based in Belgium</h3>
             </div>
-            <div className={`${dark? 'text-white' : 'text-black'} flex justify-between text-xl mx-12 items-end`}>
+            <div className={`${dark? 'text-white' : 'text-black'} grid grid-rows-2 grid-cols-4 justify-between text-xl items-end space-y-4 space-x-4`}>
               {languages.map((lang, index) => {
                 return(
-                  <p key={index} className={`${dark? 'text-white' : 'text-black'} text-xl`}>{lang}</p>
+                  <p key={index} className={`${dark? 'text-white' : 'text-black'} text-sm md:text-xl`}>{lang}</p>
                 )
               })}
             </div>
           </div>
-          <div className="flex items-center relative h-30">
-            <Image src={personalimage} layout="fill" placeholder="blur" objectFit="cover" className="rounded" />
+          <div className="hidden md:block md:my-auto md:relative row-start-1 md:col-start-1">
+            <Image src={personalimage} layout="responsive" placeholder="blur" className="rounded" />
           </div>
         </section>
       </main>
