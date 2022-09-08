d34c50d32a000ec6a93182eefc8b08cb27d94f27
Toby35 <toby.bostoen123@gmail.com>
2022-09-08 14:42:12 +0200

    testing for sendgrid

--- functions/sendmail.ts
+++ functions/sendmail.ts
@@ -4,15 +4,15 @@ exports.handler = async (event: any, context: any, callback: any) => {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
     const mail_to_send = {
-        from: "toby.botport@gmail.com",
-        to: "toby.botport@gmail.com",
-        subject: "Dit is een test",
+        from: 'toby.botport@gmail.com',
+        to: 'toby.botport@gmail.com',
+        subject: 'Rapport over de beweegvriendelijkheid van jouw stad of gemeente',
         templateId: 'd-7519de743bd444f499d2ec997c8e3a9d',
         dynamicTemplateData: {
-            subject: "Subject test",
-            message: "Message test",
-            mail: "Mail test",
-            name: "Name test"
+            message: 'Message testing',
+            subject: 'Subject testing',
+            name: 'Name testing',
+            mail: 'Mail testing'
         }
     }
 
