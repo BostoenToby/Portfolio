6061f8a70bf62d053eecb5582cf28b26fad1793a
Toby35 <toby.bostoen123@gmail.com>
2022-09-18 22:17:06 +0200

    added security headers to site

--- next.config.js
+++ next.config.js
@@ -4,4 +4,34 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; img-src 'self'; script-src 'self'"
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: "camera=(); battery=(); geolocation=(); microphone=()"
+          }
+        ]
+      }
+    ]
+  }
+}
