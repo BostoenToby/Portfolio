32e3723bded2bae501b988898aec765155fc369b
Toby35 <toby.bostoen123@gmail.com>
2022-09-18 22:28:34 +0200

    added more headers

--- next.config.js
+++ next.config.js
@@ -4,33 +4,43 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; img-src 'self'; script-src 'self'"
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: "camera=(); battery=(); geolocation=(); microphone=()"
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  }
+]
+
 module.exports = {
   async headers() {
     return [
       {
         source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; font-src 'self' 'https://fonts.googleapis.com'; img-src 'self'; script-src 'self'"
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-          {
-            key: 'Permissions-Policy',
-            value: "camera=(); battery=(); geolocation=(); microphone=()"
-          }
-        ]
+        headers: securityHeaders,
       }
     ]
   }
